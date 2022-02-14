f4caa4dba06e2e59ad1662e81da7e5e25a20bd3b
jad-rodriguez <jaylordrodriguez@gmail.com>
2022-02-15 10:15:22 +1300

    add Nav component to global

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,18 +6,20 @@ import { Link } from 'react-router-dom'
 import Home from './Home'
 import Journal from './Journal'
 import ShowEntries from './ShowEntries'
+import Nav from './Nav'
 
 
 function App () {  
   return (
     <>
-      <div>
+      {/* <div> */}
+        <Nav />
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/journal' element={<Journal />}/>
           <Route path='/entries' element={<ShowEntries />}/>
         </Routes>
-      </div>
+      {/* </div> */}
     </>
   )
 }
