1e87e7c76252ac3ccafd0a96044fcfbf99b9f2a6
jad-rodriguez <jaylordrodriguez@gmail.com>
2022-02-14 15:22:36 +1300

    add a save_entry case in the journal reducer to add new data

--- client/reducers/journal.js
+++ client/reducers/journal.js
@@ -1,8 +1,14 @@
+import { RECEIVE_ENTRIES, SAVE_ENTRY } from '../actions'
 
 function journalPosts (state = [], action) {
     switch (action.type) {
         case RECEIVE_ENTRIES:
             return action.entries
+
+        case SAVE_ENTRY:
+            const allIds = state.map(entries => entries.id)
+            const maxId = Match.max(...allIds)
+            return [...state, { id: maxId + 1, newEntryData: action.entryData}]
         default:
             return state
     }
